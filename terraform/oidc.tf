df2aa37b3d97b082ab0978b8e90699e1ecab9e72
mugnatto <gustavo@mugnatto.com.br>
2025-06-10 21:33:15 -0300

    update GitHub Actions workflow to include audience for AWS role assumption and enhance OIDC provider configuration

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,3 +1,13 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
 resource "aws_iam_role" "github_actions" {
   name = "gha-deploy-role"
 
@@ -7,11 +17,14 @@ resource "aws_iam_role" "github_actions" {
       {
         Effect: "Allow",
         Principal: {
-          Federated: "arn:aws:iam::${var.AWS_ACCOUNT_ID}:oidc-provider/token.actions.githubusercontent.com"
+          Federated: aws_iam_openid_connect_provider.github_actions.arn
         },
         Action: "sts:AssumeRoleWithWebIdentity",
         Condition: {
-          StringLike: {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          },
+          StringLike = {
             "token.actions.githubusercontent.com:sub": "repo:mugnatto/rumoaprocedenica:*"
           }
         }
